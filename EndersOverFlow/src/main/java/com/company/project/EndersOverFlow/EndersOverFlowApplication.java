c230d1fd5d5193eef1ddd2e927907626862be288
kr670356@amway.com <wjdehd7991@gmail.com>
2021-01-25 14:21:52 +0900

    최초 개발환경 세팅 완료

---
+++ EndersOverFlow/src/main/java/com/company/project/EndersOverFlow/EndersOverFlowApplication.java
@@ -0,0 +1,13 @@
+package com.company.project.EndersOverFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndersOverFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndersOverFlowApplication.class, args);
+	}
+
+}
---
+++ EndersOverFlow/src/main/java/com/company/project/EndersOverFlow/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.company.project.EndersOverFlow;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EndersOverFlowApplication.class);
+	}
+
+}
