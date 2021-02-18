34d7d6a252beb320ed16b2cbcb4aa826153d14e7
jd6186 <wjdehd7991@gmail.com>
2021-02-18 09:33:56 +0900

    METATDATA 삭제 및 변경 내용 저장

--- EndersOverFlow/src/main/java/com/company/project/EndersOverFlow/controller/MemberController.java
+++ EndersOverFlow/src/main/java/com/company/project/EndersOverFlow/controller/MemberController.java
@@ -1,27 +1,27 @@
 package com.company.project.EndersOverFlow.controller;
 
-import org.slf4j.Logger; 
-import org.slf4j.LoggerFactory; 
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus; 
-import org.springframework.http.MediaType; 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import com.company.project.EndersOverFlow.model.Member;
-
-import javax.servlet.http.HttpServletRequest; 
-import java.util.List; 
-import java.util.Optional; 
+import com.company.project.EndersOverFlow.model.Member; 
 
 @Controller
 @RequestMapping("member")
