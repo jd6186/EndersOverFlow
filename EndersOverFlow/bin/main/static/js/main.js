f5ed12158a650ad9913e15474d7a30d080f7aa26
KR620619 <KR620619@amway.com>
2021-02-23 19:58:28 +0900

    컴포넌트 분리 및 전체 리펙토링

---
+++ EndersOverFlow/bin/main/static/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/*<![CDATA[*/
+	function openToggle() {
+		const mo_header = document.getElementById("mo_h");
+		mo_header.style.display = mo_header.style.display === "block" ? "none"
+				: "block"
+	}
+/*]]>*/
